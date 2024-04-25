6d76e9f82aa3b768f3910be7d5bb8d67f7f49c99
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-04-25 16:50:37 +0700

    add library

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
 const { engine } = require('express-handlebars');
 const morgan = require('morgan');
 const { log } = require('console');
 const exp = require('constants');
-const app = express()
-const port = 3000
-const route = require('./routes/index')
+const app = express();
+const port = 3000;
+const route = require('./routes/index');
 
 // Static file
 app.use(express.static('public'));
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'))
+app.set('views', path.join(__dirname, 'resources\\views'));
 
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+    console.log(`Example app listening on port http://localhost:${port}`);
+});
