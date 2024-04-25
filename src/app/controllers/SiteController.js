216a149782c9f814403661b20af403c365e9addb
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-04-26 00:44:13 +0700

    build Course List and Course Detail

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,26 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose')
 
 class SiteController {
     // [GET] /
     index(req, res) {
+        // Course.find({})
+        //     .exec()
+        //     .then((courses) => {
+        //         res.json(courses);
+        //     })
+        //     .catch((err) => {
+        //         res.status(400).json({ error: 'ERROR!!!' });
+        //     });
+
         Course.find({})
             .exec()
             .then((courses) => {
-                res.json(courses);
+                // courses = courses.map(course => course.toObject())
+                res.render('home', {
+                    title: 'HOME PAGE',
+                    courses: mutipleMongooseToObject(courses),
+                });
             })
             .catch((err) => {
                 res.status(400).json({ error: 'ERROR!!!' });
