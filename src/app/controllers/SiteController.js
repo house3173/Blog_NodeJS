821e781c49b342b63ff7fdf9827e7f754a41e26e
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-04-25 15:14:07 +0700

    Routes and Controller

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,15 @@
+
+class SiteController {
+
+    // [GET] /
+    index(req, res) {
+        res.render('Home');
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.render('search')
+    }
+}
+
+module.exports = new SiteController;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const express = require('express')
 const { engine } = require('express-handlebars');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const { log } = require('console');
+const exp = require('constants');
 const app = express()
 const port = 3000
+const route = require('./routes')
 
 // Static file
 app.use(express.static('public'));
@@ -18,13 +21,12 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
