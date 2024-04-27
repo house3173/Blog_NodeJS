291111d6076307e41e5ff2fe6c388ae1ed46cf1e
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-04-27 22:29:03 +0700

    build Update  course

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const morgan = require('morgan');
 const { log } = require('console');
+const methodOverride = require('method-override');
 const exp = require('constants');
 const app = express();
 const port = 3000;
@@ -19,11 +20,17 @@ app.use(express.static('public'));
 // HTTP logger
 app.use(morgan('combined'));
 
+// method override use POST as DELETE/PUT/PATCH
+app.use(methodOverride('_method'));
+
 // Template engine
 app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a,b) => a+b,
+        }
     }),
 );
 app.set('view engine', 'hbs');
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,6 +5,8 @@ const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
+router.post('/:id', courseController.update);
+router.get('/:id/edit', courseController.edit);
 router.get('/:slug', courseController.show);
 
 
