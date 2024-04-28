ff28b6b8c29b88ac6d54f6380eab01f04d3dbad8
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-04-28 15:58:07 +0700

    Use count number of deleted courses

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,14 +5,31 @@ class MeController {
 
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => {
+
+        // Course.countDocuments({})
+        //     .then(count => {
+        //         console.log(count);}
+        //     )
+        //     .catch(next)
+
+        Promise.all([Course.find({}), Course.countDocuments({})])
+            .then(([courses, count]) => {
                 const haveCourses = courses.filter(course => !(course.deleted === true));
                 res.render('me/stored-courses', {
+                    deletedCount: count - haveCourses.length,
                     courses: mutipleMongooseToObject(haveCourses)
-                }
-            )})
-            .catch(next);
+                })}
+            )
+            .catch(next)
+
+        // Course.find({})
+        //     .then(courses => {
+        //         const haveCourses = courses.filter(course => !(course.deleted === true));
+        //         res.render('me/stored-courses', {
+        //             courses: mutipleMongooseToObject(haveCourses)
+        //         }
+        //     )})
+        //     .catch(next);
     }
 
     // [GET] /me/trash/courses
