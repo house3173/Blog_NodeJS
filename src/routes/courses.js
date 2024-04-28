040d637d63122e1a5afdf08eca10cdf331465130
Hau Trinh Van <trinhvanhau2003@gmail.com>
2024-04-28 17:25:51 +0700

    checkbox Select action with many items

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,6 +5,7 @@ const courseController = require('../app/controllers/CourseController');
 
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
+router.post('/handle-form', courseController.handleForm);
 router.post('/:id/update', courseController.update);
 router.patch('/:id/restore', courseController.restore);
 router.delete('/:id/delete', courseController.delete);
